29c45bf74c8fac1593eda5d3cb55bf61c4837a54
tenobu <bizan09@bizankomumakku09-no-MacBook-Air.local>
2014-09-08 14:20:09 +0900

    初期化

--- Onsei/NTViewController.m
+++ Onsei/NTViewController.m
@@ -8,7 +8,14 @@
 
 #import "NTViewController.h"
 
+#import <AVFoundation/AVFoundation.h>
+
 @interface NTViewController ()
+{
+
+	AVSpeechSynthesizer *speechSynthesizer;
+
+}
 
 @end
 
@@ -16,14 +23,40 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+    
+	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+
+	// データを文字列に変換
+	/*NSString *str = [[NSString alloc]initWithData: data
+										 encoding: NSUTF8StringEncoding];*/
+	
+	NSString *str = @"こんにちわ     ";
+	
+	NSLog(@"%@", str);
+	
+	AVSpeechUtterance      *utterance = [AVSpeechUtterance speechUtteranceWithString: str];
+	AVSpeechSynthesisVoice *JVoice    = [AVSpeechSynthesisVoice voiceWithLanguage: @"ja-JP"];
+	
+	// 音声の日本語化
+	utterance.voice =  JVoice;
+	
+	NSLog( @"%@", str );
+	// NSLog( @"bool_AudioResult == %d", bool_AudioResult );
+	
+	// 音声の発生
+	[speechSynthesizer speakUtterance: utterance];
+	
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
+    
+	[super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+
 }
 
 @end
